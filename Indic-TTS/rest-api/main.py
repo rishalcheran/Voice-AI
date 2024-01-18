f33e3e8a6c82e5378ac9abd67544e4ce140f6f69
rishal <rishalcheran@gmail.com>
2024-01-18 23:34:17 +0530

    first commit

---
+++ Indic-TTS/rest-api/main.py
@@ -0,0 +1,71 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
+from TTS.utils.synthesizer import Synthesizer
+
+from src.inference import TextToSpeechEngine
+from src.models.request import TTSRequest
+
+SUPPORTED_LANGUAGES = {
+    'as' : "Assamese - অসমীয়া",
+    'bn' : "Bangla - বাংলা",
+    'brx': "Boro - बड़ो",
+    'en' : "Indian English",
+    'gu' : "Gujarati - ગુજરાતી",
+    'hi' : "Hindi - हिंदी",
+    'kn' : "Kannada - ಕನ್ನಡ",
+    'ml' : "Malayalam - മലയാളം",
+    'mni': "Manipuri - মিতৈলোন",
+    'mr' : "Marathi - मराठी",
+    'or' : "Oriya - ଓଡ଼ିଆ",
+    'pa' : "Punjabi - ਪੰਜਾਬੀ",
+    'raj': "Rajasthani - राजस्थानी",
+    'ta' : "Tamil - தமிழ்",
+    'te' : "Telugu - తెలుగు",
+}
+
+models = {}
+for lang in SUPPORTED_LANGUAGES:
+    models[lang]  = Synthesizer(
+        tts_checkpoint=f'models/fastpitch/v1/{lang}/best_model.pth',
+        tts_config_path=f'models/fastpitch/v1/{lang}/config.json',
+        tts_speakers_file=f'models/fastpitch/v1/{lang}/speakers.pth',
+        # tts_speakers_file=None,
+        tts_languages_file=None,
+        vocoder_checkpoint=f'models/hifigan/v1/{lang}/best_model.pth',
+        vocoder_config=f'models/hifigan/v1/{lang}/config.json',
+        encoder_checkpoint="",
+        encoder_config="",
+        use_cuda=True,
+    )
+    print(f"Synthesizer loaded for {lang}.")
+    print("*"*100)
+
+engine = TextToSpeechEngine(models)
+
+api = FastAPI()
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    # allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@api.get("/supported_languages")
+def get_supported_languages():
+    return SUPPORTED_LANGUAGES
+
+@api.get("/")
+def homepage():
+    return "AI4Bharat Text-To-Speech API"
+
+@api.post("/")
+async def batch_tts(request: TTSRequest, response: Response):
+    return engine.infer_from_request(request)
+
+if __name__ == "__main__":
+    # uvicorn main:api --host 0.0.0.0 --port 5050 --log-level info
+    uvicorn.run("main:api", host="0.0.0.0", port=5050, log_level="info")
---
+++ Indic-TTS/rest-api/src/inference.py
@@ -0,0 +1,113 @@
+import io
+import base64
+import numpy as np
+import traceback
+
+from TTS.utils.synthesizer import Synthesizer
+from aksharamukha.transliterate import process as aksharamukha_xlit
+from scipy.io.wavfile import write as scipy_wav_write
+
+
+from .models.common import Language
+from .models.request import TTSRequest
+from .models.response import AudioFile, AudioConfig, TTSResponse, TTSFailureResponse
+from .utils.text import TextNormalizer
+from .utils.paragraph_handler import ParagraphHandler
+
+from src.postprocessor import PostProcessor
+
+class TextToSpeechEngine:
+    def __init__(
+        self,
+        models: dict,
+        allow_transliteration: bool = True,
+        enable_denoiser: bool = True,
+    ):
+        self.models = models
+        if allow_transliteration:
+            from ai4bharat.transliteration import XlitEngine
+            self.xlit_engine = XlitEngine(list(models), beam_width=6)
+
+        self.text_normalizer = TextNormalizer()
+        self.orig_sr = 22050 # model.output_sample_rate
+        self.enable_denoiser = enable_denoiser
+        if enable_denoiser:
+            from src.postprocessor import Denoiser
+            self.denoiser = Denoiser(self.orig_sr, self.target_sr)
+            self.target_sr = 16000
+        else:
+            self.target_sr = self.orig_sr
+        
+        self.post_processor = PostProcessor(self.target_sr)
+        self.paragraph_handler = ParagraphHandler()
+
+    def concatenate_chunks(self, wav: np.ndarray, wav_chunk: np.ndarray):
+        if type(wav_chunk) != np.ndarray:
+            wav_chunk = np.array(wav_chunk)
+        if wav is None:
+            return wav_chunk
+        return np.concatenate([wav, wav_chunk])
+
+    def infer_from_request(
+        self,
+        request: TTSRequest,
+        transliterate_roman_to_native: bool = True
+    ) -> TTSResponse:
+
+        config = request.config
+        lang = config.language.sourceLanguage
+        gender = config.gender
+
+        if lang not in self.models:
+            return TTSFailureResponse(status_text="Unsupported language!")
+        
+        if lang == "brx" and gender == "male":
+            return TTSFailureResponse(status_text="Sorry, `male` speaker not supported for this language!")
+        
+        output_list = []
+
+        for sentence in request.input:
+            wav = self.infer_from_text(sentence.source, lang, gender, transliterate_roman_to_native=transliterate_roman_to_native)
+            byte_io = io.BytesIO()
+            scipy_wav_write(byte_io, self.target_sr, wav)
+            encoded_bytes = base64.b64encode(byte_io.read())
+            encoded_string = encoded_bytes.decode()
+            speech_response = AudioFile(audioContent=encoded_string)
+            
+            output_list.append(speech_response)
+
+        audio_config = AudioConfig(language=Language(sourceLanguage=lang))
+        return TTSResponse(audio=output_list, config=audio_config)
+    
+    def infer_from_text(
+        self,
+        input_text: str,
+        lang: str,
+        speaker_name: str,
+        transliterate_roman_to_native: bool = True
+    ) -> np.ndarray:
+        try:        
+            input_text = self.text_normalizer.normalize_text(input_text, lang)
+            wav = None
+            paragraphs = self.paragraph_handler.split_text(input_text)
+            for paragraph in paragraphs:
+                if transliterate_roman_to_native and lang != 'en':
+                    paragraph = self.transliterate_sentence(paragraph, lang)
+                if lang == "mni":
+                    # TODO: Delete explicit-schwa
+                    paragraph = aksharamukha_xlit("MeeteiMayek", "Bengali", paragraph)               
+                wav_chunk = self.models[lang].tts(paragraph, speaker_name=speaker_name, style_wav="")
+                if self.enable_denoiser:
+                    wav_chunk = self.denoiser.denoise(wav_chunk)
+                wav_chunk = self.post_processor.process(wav_chunk, lang, speaker_name)
+                wav = self.concatenate_chunks(wav, wav_chunk)
+            return wav
+        except:
+            traceback.print_exc()
+            return np.zeros(1)
+
+    def transliterate_sentence(self, input_text, lang):
+        if lang == "raj":
+            lang = "hi" # Approximate
+        
+        return self.xlit_engine.translit_sentence(input_text, lang)
